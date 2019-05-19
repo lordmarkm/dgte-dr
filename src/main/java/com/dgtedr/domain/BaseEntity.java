93ec25a0280b53db9c7bc94231c5f033c0567d41
Mark Martinez <mbmartinez@exist.com>
2019-05-19 23:09:03 +0800

    Working person DB

--- src/main/java/com/dgtedr/domain/BaseEntity.java
+++ src/main/java/com/dgtedr/domain/BaseEntity.java
@@ -1,5 +1,7 @@
 package com.dgtedr.domain;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Data;
 
@@ -22,5 +26,13 @@ public class BaseEntity {
     @Column(name = "deleted")
     @Type(type = "yes_no")
     private boolean deleted;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
 
 }
