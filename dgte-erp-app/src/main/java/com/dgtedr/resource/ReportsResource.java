aab6288d495541512b0d9abb37afbfa56e87fa4e
mark.martinez <mbmartinez@exist.com>
2019-08-05 16:06:28 +0800

    Profit and loss service, profit & loss dto

--- dgte-erp-app/src/main/java/com/dgtedr/resource/ReportsResource.java
+++ dgte-erp-app/src/main/java/com/dgtedr/resource/ReportsResource.java
@@ -1,13 +1,19 @@
 package com.dgtedr.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dgtedr.dto.ProfitAndLossDto;
+
 @RestController
 @RequestMapping("/reports")
 public class ReportsResource {
+
+    @Autowired
+    private ProfitAndLossService profitAndLossService;
 
     @GetMapping("/profit-and-loss")
     public ResponseEntity<ProfitAndLossDto> profitAndLoss() {
