35445f9d975c7187758f28201d1a446a6301b5ba
mark.martinez <mbmartinez@exist.com>
2019-10-29 16:03:41 +0800

    docker config & add dist folder to gitignore

--- dgte-erp/dgte-erp-games-ui/src/environments/environment.prod.ts
+++ dgte-erp/dgte-erp-games-ui/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1'
+  apiUrl: '/api/v1',
+  firebase: {
+      apiKey: 'AIzaSyDQ-yXoIPjgh2BP1rSUKMTaDkPOcbl9sXo',
+      authDomain: 'kaiten-game.firebaseapp.com'
+  }
 };
