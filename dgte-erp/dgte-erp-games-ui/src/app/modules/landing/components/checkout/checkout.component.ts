cc75082c78c496995d3346574ea9f04bfe0eba22
mark.martinez <mbmartinez@exist.com>
2019-10-30 15:47:24 +0800

    some checkout work

--- dgte-erp/dgte-erp-games-ui/src/app/modules/landing/components/checkout/checkout.component.ts
+++ dgte-erp/dgte-erp-games-ui/src/app/modules/landing/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ShoppingCartService } from '@games/core/services';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { AuthService } from '@games/core/services';
+import { ShoppingCart } from '@games/shared/models';
 
 @Component({
   selector: 'games-checkout',
@@ -8,19 +11,37 @@ import { ShoppingCartService } from '@games/core/services';
 })
 export class CheckoutComponent implements OnInit, OnDestroy {
   public isLoading = false;
-  public shoppingCart;
+  public shoppingCart: ShoppingCart;
   private shoppingCartSub;
+  public displayName: string;
+  public displayImage: string;
+  private authStateSub;
 
-  constructor(private shoppingCartService: ShoppingCartService) { }
+  constructor(private shoppingCartService: ShoppingCartService,
+    private afAuth: AngularFireAuth,
+    private authService: AuthService) { }
 
   ngOnInit() {
     this.shoppingCartSub = this.shoppingCartService.shoppingCart.subscribe(shoppingCart => {
       this.shoppingCart = shoppingCart;
     });
+    this.authStateSub = this.afAuth.authState.subscribe(auth => {
+        if (auth) {
+            this.displayName = auth['displayName'];
+            this.displayImage = auth['photoURL'];
+        } else {
+            delete this.displayName;
+            delete this.displayImage;
+        }
+    });
   }
 
   ngOnDestroy() {
     this.shoppingCartSub.unsubscribe();
+    this.authStateSub.unsubscribe();
   }
 
+  public firebaseLogin() {
+      this.authService.FacebookAuth();
+  }
 }
