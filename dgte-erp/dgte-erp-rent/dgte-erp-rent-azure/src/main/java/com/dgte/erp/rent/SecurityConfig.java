6169983bf2dade8b3d2d200f61610e0fae4c7f12
Mark Martinez <mbmartinez@exist.com>
2020-01-07 09:35:17 +0800

    working AD

---
+++ dgte-erp/dgte-erp-rent/dgte-erp-rent-azure/src/main/java/com/dgte/erp/rent/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.dgte.erp.rent;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
+
+    public SecurityConfig(OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService) {
+        this.oidcUserService = oidcUserService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .oidcUserService(oidcUserService);
+    }
+}
